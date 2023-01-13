237936e3cc4d005fd466cc82d6c06bd207463cac
MSI\Egih Sugiatna <egsudotnet@gmail.com>
2023-01-13 20:12:06 +0700

    inisiasi

---
+++ android/app/src/main/kotlin/com/example/oop_praktik_reastaurant/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.oop_praktik_reastaurant
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData( 
+        primarySwatch: Colors.purple,
+      ), 
+      home: Home()
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hi, Silahkan pilih pesanan anda!"),
+      ),
+    );
+  }
+}
