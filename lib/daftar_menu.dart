99b16faeb9b7320d564ecbb2898f533d90f4314a
MSI\Egih Sugiatna <egsudotnet@gmail.com>
2023-01-13 20:50:30 +0700

    tambah class daftar menu

---
+++ lib/daftar_menu.dart
@@ -0,0 +1,13 @@
+class DaftarMenu{
+  String name;
+
+  DaftarMenu({
+    required this.name
+  }); 
+}
+
+var DaftarMenus = [
+  DaftarMenu(name: "Bakso"),
+  DaftarMenu(name: "Nasi Goreng"),
+  DaftarMenu(name: "Pecel Lele"),
+];
